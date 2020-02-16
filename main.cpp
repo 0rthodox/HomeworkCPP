68517b0e5639491cf017a9859ead16d09eb8d535
0rthodox <long.popov@gmail.com>
2020-02-16 22:47:24 +0300

    Provided explanation of smallness

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@ int main() {
 	std::cout << "This line will be definitely printed" << "\n"
 		<< "and this line too" << std::endl;
 
-	std::cout << "Thus we can allocate a very small vector" << std::endl;
+	std::cout << "Thus we can allocate a small vector" << std::endl; //12 GB is small enough, huh?
 	std::vector<int64_t> smallVector(12884901888, static_cast<int64_t>(1));
 	std::cout << "Vector allocated successfully! (Or not?)" << std::endl;
 
