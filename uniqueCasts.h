ef195c7f4edfb5a7bf30258a772b368357d842df
0rthodox <long.popov@gmail.com>
2020-02-22 14:22:16 +0300

    unique casts header

---
+++ uniqueCasts.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <memory>
+
+#define NODISCARD [[nodiscard]]
+
+template <typename Dst, typename Src>
+NODISCARD std::unique_ptr<Dst> static_pointer_cast(std::unique_ptr<Src>&& sourcePointer) {
+	const auto convertedPtr = static_cast<typename std::unique_ptr<Dst>::element_type*>(sourcePointer.release());
+	return std::unique_ptr<Dst>(convertedPtr, sourcePointer.get_deleter());
+}
+
+template <typename Dst, typename Src>
+NODISCARD std::unique_ptr<Dst> dynamic_pointer_cast(std::unique_ptr<Src>&& sourcePointer) {
+	const auto convertedPtr = dynamic_cast<typename std::unique_ptr<Dst>::element_type*>(sourcePointer.release());
+	if (convertedPtr) {
+		return std::unique_ptr<Dst>(convertedPtr);
+	} else {
+		return std::unique_ptr<Dst>();
+	}
+}
+
+template <typename Dst, typename Src>
+NODISCARD std::unique_ptr<Dst> const_pointer_cast(std::unique_ptr<Src>&& sourcePointer) {
+	const auto convertedPtr = const_cast<typename std::unique_ptr<Dst>::element_type*>(sourcePointer.release());
+	return std::unique_ptr<Dst>(convertedPtr, sourcePointer.get_deleter());
+}
+
+template <typename Dst, typename Src>
+NODISCARD std::unique_ptr<Dst> reinterpret_pointer_cast(std::unique_ptr<Src>&& sourcePointer) {
+	const auto convertedPtr = reinterpret_cast<typename std::unique_ptr<Dst>::element_type*>(sourcePointer.release());
+	return std::unique_ptr<Dst>(convertedPtr, sourcePointer.get_deleter());
+}
