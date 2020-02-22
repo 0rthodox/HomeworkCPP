5bddde43cec9d23876833da79243b701ddf65439
0rthodox <long.popov@gmail.com>
2020-02-22 14:29:58 +0300

    fix

--- Sem2/uniqueCasts.h
+++ Sem2/uniqueCasts.h
@@ -1,16 +1,20 @@
 #pragma once
+
+#ifndef _UNIQUE_CASTS_
+#define _UNIQUE_CASTS_
+
 #include <memory>
 
 #define NODISCARD [[nodiscard]]
 
 template <typename Dst, typename Src>
-NODISCARD std::unique_ptr<Dst> static_pointer_cast(std::unique_ptr<Src>&& sourcePointer) {
+NODISCARD std::unique_ptr<Dst> static_pointer_cast(std::unique_ptr<Src>& sourcePointer) {
 	const auto convertedPtr = static_cast<typename std::unique_ptr<Dst>::element_type*>(sourcePointer.release());
 	return std::unique_ptr<Dst>(convertedPtr, sourcePointer.get_deleter());
 }
 
 template <typename Dst, typename Src>
-NODISCARD std::unique_ptr<Dst> dynamic_pointer_cast(std::unique_ptr<Src>&& sourcePointer) {
+NODISCARD std::unique_ptr<Dst> dynamic_pointer_cast(std::unique_ptr<Src>& sourcePointer) {
 	const auto convertedPtr = dynamic_cast<typename std::unique_ptr<Dst>::element_type*>(sourcePointer.release());
 	if (convertedPtr) {
 		return std::unique_ptr<Dst>(convertedPtr);
@@ -20,13 +24,15 @@ NODISCARD std::unique_ptr<Dst> dynamic_pointer_cast(std::unique_ptr<Src>&& sourc
 }
 
 template <typename Dst, typename Src>
-NODISCARD std::unique_ptr<Dst> const_pointer_cast(std::unique_ptr<Src>&& sourcePointer) {
+NODISCARD std::unique_ptr<Dst> const_pointer_cast(std::unique_ptr<Src>& sourcePointer) {
 	const auto convertedPtr = const_cast<typename std::unique_ptr<Dst>::element_type*>(sourcePointer.release());
 	return std::unique_ptr<Dst>(convertedPtr, sourcePointer.get_deleter());
 }
 
 template <typename Dst, typename Src>
-NODISCARD std::unique_ptr<Dst> reinterpret_pointer_cast(std::unique_ptr<Src>&& sourcePointer) {
+NODISCARD std::unique_ptr<Dst> reinterpret_pointer_cast(std::unique_ptr<Src>& sourcePointer) {
 	const auto convertedPtr = reinterpret_cast<typename std::unique_ptr<Dst>::element_type*>(sourcePointer.release());
 	return std::unique_ptr<Dst>(convertedPtr, sourcePointer.get_deleter());
 }
+
+#endif _UNIQUE_CASTS_
