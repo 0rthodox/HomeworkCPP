c6b2bb2d797c273933f442930f0fdb2050096fcb
0rthodox <long.popov@gmail.com>
2020-03-12 02:40:17 +0300

    task5

---
+++ Sem5/task5.cpp
@@ -0,0 +1,177 @@
+#include <numeric>
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <iterator>
+#include <random>
+#include <chrono>
+#include "RandomGenerator.h"
+
+#define RANGE(s) s.begin(), s.end()
+
+#define PRINTC std::cout <<
+
+#define EOL << std::endl
+
+#define PRINTC_INT_RANGE(s) std::copy(RANGE(s), std::ostream_iterator<int>(std::cout, " "))
+
+template <typename T>
+std::ostream& operator<< (std::ostream& ostr, const std::vector<T>& collection) {
+	ostr << '{';
+	auto first = true;
+	for (const auto& element : collection) {
+		if (first) {
+			first = false;
+		}
+		else {
+			ostr << ' ';
+		}
+		ostr << element;
+	}
+	ostr << '}';
+	return ostr;
+}
+
+int main() {
+
+	//TASK 1
+	PRINTC 1 EOL;
+	std::vector<int> sequence;
+	sequence.resize(10);
+	std::iota(RANGE(sequence), 1);
+	PRINTC sequence EOL;
+
+	//TASK 2
+	PRINTC 2 EOL;
+	PRINTC "Additional numbers:" EOL;
+	std::copy(std::istream_iterator<int>(std::cin), std::istream_iterator<int>(), std::back_inserter(sequence));
+	PRINTC sequence EOL;
+
+	//TASK 3
+	PRINTC 3 EOL;
+	auto seed = std::chrono::steady_clock::now().time_since_epoch().count();
+	std::shuffle(RANGE(sequence),
+		std::default_random_engine(seed));
+	PRINTC sequence EOL;
+
+	//TASK 4
+	PRINTC 4 EOL;
+	//std::sort(RANGE(sequence));
+	sequence.erase(std::unique(RANGE(sequence)), sequence.end());
+	PRINTC sequence EOL;
+
+	//TASK 5
+	PRINTC 5 EOL;
+	auto oddCount = std::count_if(RANGE(sequence), [](const auto& x) {
+		return (x % 2);
+		});
+	std::cout << "Odd numbers: " << std::move(oddCount) << std::endl;
+
+	//TASK 6
+	PRINTC 6 EOL;
+	auto mmElements = std::minmax_element(RANGE(sequence));
+	std::cout << "Min = " << *mmElements.first << ", max = " << *mmElements.second << std::endl;
+
+	//TASK 7
+	PRINTC 7 EOL;
+	PRINTC "Prime number: " << *std::find_if(RANGE(sequence), [](const auto& x) {
+		for (auto i = 1; i < (x + 1) / 2; ++i) {
+			if (!(x % i)) {
+				return true;
+			}
+			return false;
+		}
+		}) EOL;
+
+	//TASK 8
+	PRINTC 8 EOL;
+	std::transform(RANGE(sequence), sequence.begin(), [](const auto& x) {
+		return x * x;
+		});
+	PRINTC sequence EOL;
+
+
+	//TASK 9
+	PRINTC 9 EOL;
+	std::mt19937 generator;
+	std::vector<int> sSequence;
+	std::generate_n(back_inserter(sSequence), sequence.size(), std::ref(generator));
+	PRINTC sSequence EOL;
+
+	//TASK 10
+	PRINTC 10 EOL;
+	PRINTC "Sum = " << std::accumulate(RANGE(sSequence), 0) EOL;
+
+	//TASK 11
+	PRINTC 11 EOL;
+	auto amount = RandomGenerator(static_cast<size_t>(1), sSequence.size() - 1).rand();
+	std::for_each_n(sSequence.begin(), amount, [](auto& x) {
+		x = 1;
+		});
+	PRINTC sSequence EOL;
+
+	//TASK 12
+	PRINTC 12 EOL;
+	std::vector<int> tSequence;
+	std::transform(RANGE(sequence), sSequence.begin(), std::back_inserter(tSequence), [](const auto& x, const auto& y) {
+		return x - y;
+		});
+	PRINTC tSequence EOL;
+	
+	//TASK 13
+	PRINTC 13 EOL;
+	std::replace_if(RANGE(tSequence), [](const auto& x) {
+		return x < 0;
+		}, 0);
+	PRINTC tSequence EOL;
+
+	//TASK 14
+	PRINTC 14 EOL;
+	tSequence.erase(std::remove_if(RANGE(tSequence), [](const auto& x) {
+		return x == 0;
+		}), tSequence.end());
+	PRINTC tSequence EOL;
+
+
+	//TASK 15
+	PRINTC 15 EOL;
+	std::reverse(RANGE(tSequence));
+	PRINTC tSequence EOL;
+
+
+	//TASK 16
+	PRINTC 16 EOL;
+	std::nth_element(tSequence.begin(), std::next(tSequence.begin(), 2), tSequence.end(), std::greater<int>());
+	PRINTC "Three greatest elements: " << tSequence[0]
+		<< ' ' << tSequence[1] << ' ' << tSequence[2] EOL;
+	
+	//TASK 17
+	PRINTC 17 EOL;
+	std::sort(RANGE(sequence));
+	std::sort(RANGE(sSequence));
+	PRINTC sequence EOL;
+	PRINTC sSequence EOL;
+
+
+	//TASK 18
+	PRINTC 18 EOL;
+	std::vector<int> fSequence;
+	std::merge(RANGE(sequence), RANGE(sSequence), std::back_inserter(fSequence));
+	PRINTC fSequence EOL;
+
+
+	//TASK 19
+	PRINTC 19 EOL;
+	auto eRange = std::equal_range(RANGE(fSequence), 1);
+	PRINTC "Should be inserted between values: " << *eRange.first << ' ' << *eRange.second EOL;
+
+	//TASK 20
+	PRINTC 20 EOL;
+	PRINTC_INT_RANGE(sequence);
+	PRINTC ' ' EOL;
+	PRINTC_INT_RANGE(sSequence);
+	PRINTC ' ' EOL;
+	PRINTC_INT_RANGE(tSequence);
+	PRINTC ' ' EOL;
+	PRINTC_INT_RANGE(fSequence);
+}
