debb5320418771cf721576814f2ba5161f64ab22
0rthodox <long.popov@gmail.com>
2020-03-26 18:20:28 +0300

    fixed programmer

--- Sem7/Programmer.hpp
+++ Sem7/Programmer.hpp
@@ -1,15 +1,15 @@
 #pragma once
+#include <boost/bimap.hpp>
+#include <iostream>
+#include "json.hpp"
 #include <string>
 #include <vector>
-#include <iostream>
 #include <unordered_map>
-#include "json.hpp"
-#include <boost/bimap.hpp>
 
 using nlohmann::json;
 
-struct Programmer {
-	enum Proficiency {
+class Programmer {
+	enum class Proficiency {
 		BEGINNER,
 		MEDIUM,
 		ADVANCED,
@@ -27,15 +27,7 @@ struct Programmer {
 		initProficiencyMap();
 	}
 	friend std::istream& operator>> (std::istream& input, Programmer& programmer);
-	json toJson() {
-		json jsonedProgrammer;
-		jsonedProgrammer["Name"] = name;
-		jsonedProgrammer["Age"] = age;
-		jsonedProgrammer["Language"] = language;
-		jsonedProgrammer["Level"] = proficiencyMap.left.at(level);
-
-		return jsonedProgrammer;
-	}
+	json toJson();
 
 	boost::bimap<Proficiency, std::string> proficiencyMap;
 	std::string name;
@@ -43,13 +35,25 @@ struct Programmer {
 	std::string language;
 	Proficiency level;
 private:
-	void initProficiencyMap() {
-		proficiencyMap.insert(boost::bimap<Proficiency, std::string>::value_type(BEGINNER, "Beginner"));
-		proficiencyMap.insert(boost::bimap<Proficiency, std::string>::value_type(MEDIUM, "Medium"));
-		proficiencyMap.insert(boost::bimap<Proficiency, std::string>::value_type(ADVANCED, "Advanced"));
-		proficiencyMap.insert(boost::bimap<Proficiency, std::string>::value_type(MASTER, "Master"));
-	}
+	void initProficiencyMap();
 };
+
+json Programmer::toJson() {
+	json jsonedProgrammer;
+	jsonedProgrammer["Name"] = name;
+	jsonedProgrammer["Age"] = age;
+	jsonedProgrammer["Language"] = language;
+	jsonedProgrammer["Level"] = proficiencyMap.left.at(level);
+
+	return jsonedProgrammer;
+}
+
+void Programmer::initProficiencyMap() {
+	proficiencyMap.insert(boost::bimap<Proficiency, std::string>::value_type(Programmer::Proficiency::BEGINNER, "Beginner"));
+	proficiencyMap.insert(boost::bimap<Proficiency, std::string>::value_type(Programmer::Proficiency::MEDIUM, "Medium"));
+	proficiencyMap.insert(boost::bimap<Proficiency, std::string>::value_type(Programmer::Proficiency::ADVANCED, "Advanced"));
+	proficiencyMap.insert(boost::bimap<Proficiency, std::string>::value_type(Programmer::Proficiency::MASTER, "Master"));
+}
 
 std::istream& operator>> (std::istream& input, Programmer& programmer) {
 	std::string level;	
