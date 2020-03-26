04b6f44b828dc8eeca1b7ac66728b78edf7b05c6
0rthodox <long.popov@gmail.com>
2020-03-27 00:45:25 +0300

    works

--- Sem6/Transliterator.h
+++ Sem6/Transliterator.h
@@ -1,77 +1,54 @@
 #pragma once
-#include <string>
-#include <unordered_map>
+
+#include <boost\locale\generator.hpp>
+#include <boost\locale\encoding.hpp>
+#include <boost\locale\util.hpp>
 #include <fstream>
 #include <iostream>
-#include <boost\locale\util.hpp>
-#include <boost\locale\encoding.hpp>
-#include <boost\locale\generator.hpp>
+#include <string>
+#include <unordered_map>
 
-#define RANGE(s) begin(s), end(s)
-
-std::wstring convert_string_to_wstring(
-	const std::string& string,
-	const std::locale& locale = std::locale())
-{
-	std::vector < wchar_t > buffer(string.size());
-
-	std::use_facet < std::ctype < wchar_t > >(locale).widen(
-		string.data(),
-		string.data() + string.size(),
-		buffer.data());
-
-	return std::wstring(buffer.data(), buffer.size());
-}
-
-std::string convert_wstring_to_string(
-	const std::wstring& wstring,
-	const std::locale& locale = std::locale())
-{
-	std::vector < char > buffer(wstring.size());
-
-	std::use_facet < std::ctype < wchar_t > >(locale).narrow(
-		wstring.data(),
-		wstring.data() + wstring.size(),
-		'?', buffer.data()); // default character
-
-	return std::string(buffer.data(), buffer.size());
+namespace borrowed {
+	std::string convert_utf_to_locale(const std::string& string);
+	std::string convert_locale_to_utf(const std::string& string);
 }
 
 
-std::string convert_locale_to_utf(const std::string& string)
-{
-	boost::locale::generator generator;
-	generator.locale_cache_enabled(true);
-
-	std::locale locale =
-		generator(boost::locale::util::get_system_locale());
-
-	return boost::locale::conv::to_utf < char >(string, locale);
-}
-
-std::string convert_utf_to_locale(const std::string& string)
-{
-	boost::locale::generator generator;
-	generator.locale_cache_enabled(true);
-
-	std::locale locale =
-		generator(boost::locale::util::get_system_locale());
-
-	return boost::locale::conv::from_utf < char >(string, locale);
-}
 
 class Transliterator {
 public:
 	Transliterator();
-	std::string operator()(const std::string& inputString) {
-		std::string u8InputString = convert_locale_to_utf(inputString);
-		std::u32string u32InputString = boost::locale::conv::utf_to_utf<char32_t>(u8InputString);
-
-	}
+	std::string operator()(const std::string&);
 
 private:
+	std::u32string transliterateU32(const std::u32string&);
 	std::unordered_map<char32_t, std::u32string> m_transliterates;
 };
+
+//Methods & Constructor implementation:
+
+std::string Transliterator::operator()(const std::string& input) {
+	return borrowed::convert_utf_to_locale(
+		boost::locale::conv::utf_to_utf<char>(
+			transliterateU32(
+				boost::locale::conv::utf_to_utf<char32_t>(
+					borrowed::convert_locale_to_utf(input)))));
+}
+
+std::u32string Transliterator::transliterateU32(const std::u32string& input) {
+	std::u32string output;
+	for (auto letter : input) {
+		auto letterIterator = m_transliterates.find(letter);
+		if (m_transliterates.end() != letterIterator) {
+			auto replacingString = letterIterator->second;
+			std::copy(replacingString.begin(), replacingString.end(), std::back_inserter(output));
+		}
+		else {
+			output.push_back(letter);
+		}
+	}
+	return output;
+}
 
 Transliterator::Transliterator() : m_transliterates({
 	{U'é', U"j"},
@@ -133,7 +110,7 @@ Transliterator::Transliterator() : m_transliterates({
 	{U'Ý', U"E'"},
 	{U'ß', U"Ya"},
 	{U'×', U"Ch"},
-	{U'Ñ', U"C"},
+	{U'Ñ', U"S"},
 	{U'Ì', U"M"},
 	{U'È', U"I"},
 	{U'Ò', U"T"},
@@ -141,3 +118,27 @@ Transliterator::Transliterator() : m_transliterates({
 	{U'Á', U"B"},
 	{U'Þ', U"Yu"},
 	}) {}
+
+//Borrowed functions
+
+std::string borrowed::convert_locale_to_utf(const std::string& string)
+{
+	boost::locale::generator generator;
+	generator.locale_cache_enabled(true);
+
+	std::locale locale =
+		generator(boost::locale::util::get_system_locale());
+
+	return boost::locale::conv::to_utf < char >(string, locale);
+}
+
+std::string borrowed::convert_utf_to_locale(const std::string& string)
+{
+	boost::locale::generator generator;
+	generator.locale_cache_enabled(true);
+
+	std::locale locale =
+		generator(boost::locale::util::get_system_locale());
+
+	return boost::locale::conv::from_utf < char >(string, locale);
+}
---
+++ Sem6/main6.cpp
@@ -0,0 +1,10 @@
+#include "Transliterator.h"
+
+int main() {
+	//Second task
+	/*system("chcp 1251");
+	std::string s;
+	std::cin >> s;
+	Transliterator transliterator;
+	std::cout << transliterator(s);*/
+}
