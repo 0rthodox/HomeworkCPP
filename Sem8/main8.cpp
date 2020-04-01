36590e0573813e41ab348f7395a958607c69aed3
0rthodox <long.popov@gmail.com>
2020-04-01 23:11:37 +0300

    fix

--- Sem8/main8.cpp
+++ Sem8/main8.cpp
@@ -4,9 +4,10 @@
 #include <numeric>
 
 int main() {
-	std::vector<int> ints(1000);
+	std::vector<int> ints(100000);
 	std::iota(ints.begin(), ints.end(), 1);
 	parForEach(ints.begin(), ints.end(), [](auto& x) {
+		std::cout << x << " ";
 		x *= x;
 		});
 	for (const auto& x : ints) {
